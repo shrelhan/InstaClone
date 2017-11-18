036a10632c23a40e0389f8b47cf830df241d116b
shrelhan <shrelhan.relhan@gmail.com>
2017-11-19 01:05:52 +0530

    InstaClon12

--- myapp/models.py
+++ myapp/models.py
@@ -1,35 +1,74 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-import uuid
 
 from django.db import models
 
-# Create your models here.
-class UserModel(models.Model):
-  objects = None
-  email = models.EmailField()
-  name = models.CharField(max_length=120)
-  username = models.CharField(max_length=120)
-  password = models.CharField(max_length=40)
-  created_on = models.DateTimeField(auto_now_add=True)
-  updated_on = models.DateTimeField(auto_now=True)
+import uuid
 
+class UserModel(models.Model):
+	objects = None
+	email = models.EmailField()
+	name = models.CharField(max_length=120)
+	username = models.CharField(max_length=120)
+	password = models.CharField(max_length=40)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
 
 class SessionToken(models.Model):
-    user = models.ForeignKey(UserModel)
-    session_token = models.CharField(max_length=255)
-    created_on = models.DateTimeField(auto_now_add=True)
-    is_valid = models.BooleanField(default=True)
+	objects = None
+	user = models.ForeignKey(UserModel)
+	session_token = models.CharField(max_length=255)
+	last_request_on = models.DateTimeField(auto_now=True)
+	created_on = models.DateTimeField(auto_now_add=True)
+	is_valid = models.BooleanField(default=True)
 
-    def create_token(self):
-        self.session_token = uuid.uuid4()
-
+	def create_token(self):
+		self.session_token = uuid.uuid4()
 
 class PostModel(models.Model):
-  user = models.ForeignKey(UserModel)
-  image = models.FileField(upload_to='user_images')
-  image_url = models.CharField(max_length=255)
-  caption = models.CharField(max_length=240)
-  created_on = models.DateTimeField(auto_now_add=True)
-  updated_on = models.DateTimeField(auto_now=True)
+  	objects = None
+	user = models.ForeignKey(UserModel)
+  	image = models.FileField(upload_to='user_images')
+  	image_url = models.CharField(max_length=255)
+  	caption = models.CharField(max_length=240)
+  	created_on = models.DateTimeField(auto_now_add=True)
+  	updated_on = models.DateTimeField(auto_now=True)
+
+	@property
+	def like_count(self):
+		return len(LikeModel.objects.filter(post=self))
+
+	@property
+	def comments(self):
+		return CommentModel.objects.filter(post=self).order_by('-created_on')
+
+
+class LikeModel(models.Model):
+	objects = None
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+
+class CommentModel(models.Model):
+	objects = None
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	comment_text = models.CharField(max_length=555)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+class clarifai_data(models.Model):
+    user = models.ForeignKey(UserModel)
+    clarifai_data = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class CategoryModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	category_text=models.CharField(max_length=255)
+
+	@property
+	def category(self):
+		return CategoryModel.objects.filter(post=self)
--- myapp/views.py
+++ myapp/views.py
@@ -1,36 +1,64 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from models import UserModel, SessionToken, PostModel
 from django.shortcuts import render, redirect
-from datetime import datetime
-from forms import SignUpForm, LoginForm, PostForm
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel , CategoryModel
 from django.contrib.auth.hashers import make_password, check_password
-from imgurpython import ImgurClient
+from datetime import timedelta,datetime
+from django.utils import timezone
 from InstaClone.settings import BASE_DIR
+
+from clarifai.rest import ClarifaiApp
+app = ClarifaiApp(api_key='d069c54dd0d74aed8bfe6ad7ea79ad13')
+from imgurpython import ImgurClient
+import json
+import codecs
+
+CLIENT_ID = 'a8aa2277a419d2e'
+CLIENT_SECRET = '48058116a0e1f2abccfa16ca4237550fe4529bcc'
 
 
 # Create your views here.
 
+
+#Function for Sign Up
 def signup_view(request):
-  global form
-  if request.method == "POST":
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-      username = form.cleaned_data['username']
-      name = form.cleaned_data['name']
-      email = form.cleaned_data['email']
-      password = form.cleaned_data['password']
-      #saving data to DB
-      user = UserModel(name=name, password=make_password(password), email=email, username=username)
-      user.save()
-      return render(request, 'success.html')
-  elif request.method == "GET":
-    form = SignUpForm()
+    global form
+    response_data = {}
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
 
-  return render(request, 'index.html', {'form' : form})
+            #Condition for usename and password length
+            if len(username) < 5:
+                response_data['message']= 'Username must have atleat 4 characters'
+
+                return render(request, 'index.html', {'form': form})
+
+            if len(password) < 6:
+                response_data['message']= 'Password must have atleast 6 character'
+
+                return render(request,'index.html', {'form': form})
+
+            # saving data to DB
+            user = UserModel(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            return render(request, 'success.html',{'name': name})
+
+            # return redirect('login/')
+    if request.method == "GET":
+        form = SignUpForm()
+
+    response_data['form'] = form
+    return render(request, 'index.html', {'form': form})
 
 
+#Function for login
 def login_view(request):
+    global form
     response_data = {}
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -54,29 +82,10 @@ def login_view(request):
         form = LoginForm()
 
     response_data['form'] = form
-    return render(request, 'login.html', response_data)
+    return render(request, 'login.html',response_data)
 
 
-def feed_view(request):
-    user = check_validation(request)
-    if user:
-        posts = PostModel.objects.all().order_by('created_on')
-        return render(request, 'feed.html', {'posts': posts})
-    else:
-        return redirect('/login/')
-
-
-# For validating the session
-def check_validation(request):
-    if request.COOKIES.get('session_token'):
-        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
-        if session:
-            return session.user
-    else:
-        return None
-
-CLIENT_ID = 'a8aa2277a419d2e'
-CLIENT_SECRET = '48058116a0e1f2abccfa16ca4237550fe4529bcc'
+#Function for post view
 def post_view(request):
     user = check_validation(request)
 
@@ -91,9 +100,22 @@ def post_view(request):
                 post.save()
 
                 path = str(BASE_DIR + '\\' + post.image.url)
+
                 client = ImgurClient(CLIENT_ID, CLIENT_SECRET)
                 post.image_url = client.upload_from_path(path, anon=True)['link']
+                clarifai_data = []
+                app = ClarifaiApp(api_key='d031a5db120a409dae6d1f9a3d9e870c')
+                model = app.models.get("general-v1.3")
+                response = model.predict_by_url(url=post.image_url)
+                file_name = 'output' + '.json'
+
+                for json_dict in response:
+                    for key, value in response.iteritems():
+                        print("key: {} | value: {}".format(key, value))
                 post.save()
+
+
+
                 return redirect('/feed/')
 
         else:
@@ -101,3 +123,100 @@ def post_view(request):
         return render(request, 'post.html', {'form': form})
     else:
         return redirect('/login/')
+
+
+#Function for Feed view
+def feed_view(request):
+    user = check_validation(request)
+    if user:
+
+        posts = PostModel.objects.all().order_by('created_on')
+
+        for post in posts:
+            existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
+            if existing_like:
+                post.has_liked = True
+
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+
+        return redirect('/login/')
+
+
+#Function for likking a post
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+            else:
+                existing_like.delete()
+            return redirect('/feed/')
+    else:
+        return redirect('/login/')
+
+
+#Functin for commenting on a post
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login')
+
+
+# For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
+    else:
+        return None
+
+
+# Function for logout
+def logout_view(request):
+    user = check_validation(request)
+    if user is not None:
+        latest_session = SessionToken.objects.filter(user=user).last()
+        if latest_session:
+            latest_session.delete()
+
+    return redirect("/login/")
+
+
+# this view show the automatic categories
+def add_category(post):
+    app = ClarifaiApp(api_key='fcfdca12d67a4af7b657c4117ea90128')
+    model = app.models.get("general-v1.3")
+    response = model.predict_by_url(url=post.image_url)
+    if response["status"]["code"]==10000:
+        if response["outputs"]:
+            if response["output"][0]["data"]:
+                if response["output"][0]["data"]["concepts"]:
+                    for index in range (0,len(response["outputs"][0]["data"]["concepts"])):
+                        category=CategoryModel(post=post,category_text=response['outputs'][0]['data']['concepts'][index]['name'])
+                        category.save()
+                else:
+                    print 'no concepts error'
+            else:
+                print 'no data list error'
+        else:
+            print 'no outtput list error'
+    else:
+        print 'response code error'
